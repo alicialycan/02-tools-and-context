290711a079dee91f938b59aba3d44428402442df
alicialycan <alicialycan@gmail.com>
2018-02-09 08:40:31 -0800

    add tests

--- lab-alicia/test/fp.test.js
+++ lab-alicia/test/fp.test.js
@@ -5,9 +5,42 @@ const expect = require('expect');
 
 describe('testing fp', () => {
 
-    const arr = [1,2,3,4,5];
-    fp.forEach(arr, el => console.log( el));
-        fp.map(arr, el => el * 2);
-    fp.filter(arr, el => el % 2 === 0);
-    fp.reduce(arr, (a,b) => a + b, 0);
+    describe('testing fp.forEach', () => {
+        let expected = [1, 2, 3, 4, 5];
+        it('should return lines of 1, 2, 3, 4, 5', () => {
+            const arr = [1, 2, 3, 4, 5];
+            let cb = console.log(cb);
+            let result = fp.forEach(arr, cb);
+            expect(result).toEqual(expected);
+        })
+    });
+
+    describe('testing fp.map', () => {
+        let expected = [2, 4, 6, 8, 10];
+        it('should multiply each integer by 2 and return [2, 4, 6, 8, 10]', () => {
+            const arr = [1, 2, 3, 4, 5];
+            let cb = (data => data * 2);
+            let result = fp.map(arr, cb);
+            expect(result).toEqual(expected);
+        })
+    });
+
+    describe('testing fp.filter', () => {
+        let expected = [2, 4];
+        it('should remove odd integers from the arr return [2, 4]', () => {
+            const arr = [1, 2, 3, 4, 5];
+            let cb = (data => data % 2);
+            let result = fp.filter(arr, cb);
+            expect(result).toEqual(expected);
+        })
+    });
+
+    describe('testing fp.reduce', () => {
+        let expected = [15];
+        it('should add all integers return a value of 15', () => {
+            let numbers = [1, 2, 3, 4, 5];
+            let result = fp.reduce(numbers, (runningValue, number) => runningValue + number, 0);
+            expect(result).toEqual(expected);
+        })
+    });
 });
